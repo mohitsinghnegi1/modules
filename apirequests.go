bd968eb44d61717155ceb5b58830c77e45d9202f
Mohit Singh Negi <mohit@bombayplay.com>
2020-11-16 11:32:04 +0530

    first commit

---
+++ apirequests.go
@@ -0,0 +1,11 @@
+package modules
+
+// ServerHeartBeat Function
+func ServerHeartBeat() string {
+	return "True"
+}
+
+// GetGameRoomServerAddress Function
+func GetGameRoomServerAddress() string {
+	return "HI"
+}
